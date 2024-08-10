851fe92997144393fbe1a3449ae03a71583498c0
Ivo-Verardo <161718019+Ivo-Verardo@users.noreply.github.com>
2024-08-10 21:12:52 +0200

    Update jean registre.py
    passé du modèle du déplacement balistique d'un point à un model tenant compte de la trainée selon le modèle de Tartaglia

--- jean registre.py
+++ jean registre.py
@@ -2,33 +2,22 @@ from tkinter import *
 from math import *
 # Constantes pour le calcul de la trajectoire
 g = 9.81  # Accélération due à la gravité (m/s^2)
-m = 0.046  # Masse d'une balle de golf (kg)
+m = 0.042  # Masse d'une balle de golf (kg)
 C_d = 0.24  # Coefficient de traînée d'une balle de golf
 C_l = 0.11 # Coefficient de portance d'une balle de golf
 ρ = 1.225  # Densité de l'air (kg/m^3)
 A = 0.0014  # Surface en coupe d'une balle de golf (m^2)
 e = 0.83 # Coefficient de restitution
-'''
-C_d*1/2*ρAv^2+mgsin(α)=ma_x
-C_l*1/2*ρAv^2+mgcos(α)=ma_y
-'''
-
-
 
 def choc(v_c, m_c) :
-    v_b = ((2*m_c*v_c)/(m_c+m))*e
-    return v_b
+    vitesse = ((2*m_c*v_c)/(m_c+m))*e
+    return vitesse
 
 def calculer_portee(v_b, a):
     # Convertir l'angle en radians
     α = radians(a)
-    # Calculer les composantes de la vitesse initiale
-    v_b_x = v_b * cos(α)
-    v_b_y = v_b * sin(α)
-    # Calculer le temps de vol
-    temps_vol = (2 * v_b_y) / g
-    # Calculer la portée de la balle de golf
-    portee = v_b_x * temps_vol
+    l = (2*m)/(ρ*A*C_d)
+    portee = 0.5*l*cos(α)*log(1+4*(v_b**2/(g*l))*sin(α))
     return portee
     
 def maj_trajectoire():
@@ -41,19 +30,29 @@ def maj_trajectoire():
     # Effacer le canevas
     canevas.delete("all")
     # Dessiner la trajectoire
-    points = []  # Liste pour stocker les coordonnées des ovales
+    points = [(50, 250)]  # Liste pour stocker les coordonnées des ovales
     y = 250
     t = 0
+    l = (2*m)/(ρ*A*C_d)
+    h = l*tan(radians(angle))
+    L = 50+l
+    H = 250 - h
+    points.append((L, H))
+    vit_2 = vitesse*2.71828**(-(sqrt(l**2+h**2)/vitesse)/l)
     while y <= 250:
-        x = 50 + vitesse * cos(radians(angle)) * t
-        y = 250 - (vitesse * t * sin(radians(angle)) - 0.5 * g * t**2)
+        
+        x = L + vit_2 * cos(radians(angle)) * t
+        y = H - vit_2 * sin(radians(angle)) * t + 0.5 * g * t**2
+        
         points.append((x, y))  
-        canevas.create_oval(x-0, y-0, x+0, y+0, fill="white")
+        canevas.create_oval(x, y, x, y, fill="white")
         canevas.create_line(50, 250, 50+portee, 250)
-        t +=0.01
+        t +=0.1
     canevas.create_line(points, fill="blue")  # Dessiner une ligne entre les ovales
     # Mettre à jour l'étiquette de portée
     etiquette_portee.config(text=f"Portée: {portee:.2f} mètres")
+        
+
 
 # Créer la fenêtre principale
 fenetre = Tk()
@@ -89,5 +88,3 @@ etiquette_portee.pack()
 
 
 fenetre.mainloop()
-
-
