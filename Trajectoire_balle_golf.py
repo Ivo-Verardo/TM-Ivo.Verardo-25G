08f4980081e2fd35a0e0c68af213d936be77c60c
Ivo-Verardo <161718019+Ivo-Verardo@users.noreply.github.com>
2024-08-30 16:59:12 +0200

    Update and rename jean registre.py to Trajectoire_balle_golf.py
    Principalement Calcul des coefficients et quelques autres modifications

--- Trajectoire_balle_golf.py
+++ Trajectoire_balle_golf.py
@@ -3,31 +3,22 @@ from math import *
 # Constantes pour le calcul de la trajectoire
 g = 9.81  # Accélération due à la gravité (m/s^2)
 m = 0.042  # Masse d'une balle de golf (kg)
-C_d = 0.24  # Coefficient de traînée d'une balle de golf
-C_l = 0.11 # Coefficient de portance d'une balle de golf
-ρ = 1.225  # Densité de l'air (kg/m^3)
+ρ = 1.095  # Densité de l'air (kg/m^3)
 A = 0.0014  # Surface en coupe d'une balle de golf (m^2)
-e = 0.83 # Coefficient de restitution
 
 def choc(v_c, m_c) :
-    vitesse = ((2*m_c*v_c)/(m_c+m))*e
+  
+    vitesse = ((2*m_c*v_c)/(m_c+m))
     return vitesse
-
-def calculer_portee(v_b, a):
-    # Convertir l'angle en radians
-    α = radians(a)
-    l = (2*m)/(ρ*A*C_d)
-    val_int = 0.5*l*cos(α)*log(1+4*(v_b**2/(g*l))*sin(α))
-    portee = val_int+(val_int*((0.6981-α)/(2.5*α)))
-    return portee
     
 def maj_trajectoire():
-    # Obtenir la vitesse et l'angle des entrées utilisateur
-    vitesse = choc(float(entree_v_c.get()),float(entree_m_c.get()))
+    # Obtenir la vitesse, la masse et l'angle des entrées utilisateur
+    vit_club = float(entree_v_c.get())
+    mas_club = float(entree_m_c.get())
     angle = float(entree_angle.get())
+    #
+    vitesse = choc(vit_club, mas_club)
     α = radians(angle)
-    # Calculer la portée de la balle de golf
-    portee = calculer_portee(vitesse, angle)
     # Effacer le canevas
     canevas.delete("all")
     # Dessiner la trajectoire
@@ -37,38 +28,39 @@ def maj_trajectoire():
     dt = 0.01
     Vx = vitesse*cos(α)
     Vy = vitesse*sin(α)
+    #calcul des coefficients
+    C_l = 2*angle/(30*mas_club*vit_club)
+    C_d = 2*C_l-0.03
+    #calcul de la trajectoire
     while y <= 250 :
-        
+        #Calcul de la force de traînée
         Fdx = -C_d*0.5*ρ*A*Vx**2
         Fdy = -C_d*0.5*ρ*A*Vy**2
-        
+        #Calcul de la force de portance
         Flx = -C_l*0.5*ρ*A*Vx**2
         Fly = C_l*0.5*ρ*A*Vy**2
-        
+        #Calcul de la force de pesanteur
         Fp = m*-g
-        
+        #Calcul de la somme des forces
         SFx = Fdx + Flx
         SFy = Fdy + Fly + Fp
-        
+        #Calcul de l'acceleration
         ax = SFx/m
         ay = SFy/m
-        
+        #Calcul du nouveau point
         x += Vx*dt
         y -= Vy*dt
-        
+        #Calcul de la nouvelle vitesse
         Vx += ax*dt
         Vy += ay*dt
-        
-        
+        #dessin du point
         points.append((x, y))
-        canevas.create_oval(x, y, x, y, fill="white")
+        canevas.create_oval(x, y, x, y, fill="blue")
         
+        portee = x-50
     canevas.create_line(points, fill="blue")  # Dessiner une ligne entre les ovales
-    canevas.create_line(50, 250, 50+portee, 250)
     # Mettre à jour l'étiquette de portée
     etiquette_portee.config(text=f"Portée: {portee:.2f} mètres")
-        
-
 
 # Créer la fenêtre principale
 fenetre = Tk()
